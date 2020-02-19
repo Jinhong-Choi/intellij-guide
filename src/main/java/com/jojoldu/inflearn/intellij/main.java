016c383db6de1073a685a349ff64d5ece4a9c7ee
Choi_Jinhong <chlwlsghd58@gmail.com>
2020-02-19 14:06:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/jojoldu/inflearn/intellij/main.java
@@ -0,0 +1,7 @@
+package com.jojoldu.inflearn.intellij;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
---
+++ src/main/java/com/jojoldu/inflearn/intellij/main2.java
@@ -0,0 +1,8 @@
+package com.jojoldu.inflearn.intellij;
+import com.sun.awt.*;
+
+public class main2 {
+    public static void main(String[] args) {
+        System.out.println("bbb");
+    }
+}
